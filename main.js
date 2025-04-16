b4f9103511bc0040fae382a47f09a1647554ef8d
FmDeveloper <fmdeveloper2@gmail.com>
2025-04-16 20:28:46 -0300

    Projeto - Jogo da forca
    Atualizacao de README.MD

--- main.js
+++ main.js
@@ -8,5 +8,25 @@ if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
   console.log('Valores inválidos. Digite números maiores que zero.');
 } else {
   const resultado = calcularImc(peso, altura);
-  console.log(`Seu IMC é ${resultado}`);
+  if (resultado <= 18.5) {
+    console.log(`Seu IMC é ${resultado}`);
+    console.log('Você está abaixo do peso ideal');
+
+  } else if (resultado > 18.5 && resultado <= 24.9) {
+    console.log(`Seu IMC é ${resultado}`);
+    console.log('Você está no peso ideal');
+  } else if (resultado > 24.9 && resultado <= 29.9) {
+    console.log(`Seu IMC é ${resultado}`);
+    console.log('Você está com sobrepeso');
+  } else if (resultado > 29.9 && resultado <= 34.9) {
+    console.log(`Seu IMC é ${resultado}`);
+    console.log('Você está com obesidade grau 1');
+  } else if (resultado > 34.9 && resultado <= 39.9) {
+    console.log(`Seu IMC é ${resultado}`);
+    console.log('Você está com obesidade grau 2');
+  } else if (resultado > 39.9) {
+    console.log(`Seu IMC é ${resultado}`);
+    console.log('Você está com obesidade grau 3');
+  } else
+    console.log("Valores inválidos. Digite números maiores que zero.");
 }
